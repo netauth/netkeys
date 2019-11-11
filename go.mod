d5c4bfe120a3b9f5e5130a0d59c9abef3e139048
Michael Aldridge <aldridge.mac@gmail.com>
2019-11-11 00:01:45 -0800

    Upgrade for NetAuth2 protocol

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
-module github.com/NetAuth/NetKeys
+module github.com/netauth/netkeys
 
 go 1.12
 
 require (
-	github.com/NetAuth/NetAuth v0.0.0-20190331030244-7acf42db8717
+	github.com/netauth/netauth v0.3.0-rc1
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.3.2
 )
--- main.go
+++ main.go
@@ -5,19 +5,22 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+	"context"
 
-	"github.com/spf13/viper"
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 
-	"github.com/NetAuth/NetAuth/pkg/client"
+	"github.com/netauth/netauth/pkg/netauth"
+	_ "github.com/netauth/netauth/pkg/netauth/memory"
 )
 
 var (
 	keyType   = pflag.String("type", "SSH", "Type of keys to print")
 	entityID  = pflag.String("ID", "", "ID to look up")
 	serviceID = pflag.String("service", "netkeys", "Service ID to send")
-	cfgfile = pflag.String("config", "", "Config file to use")
-	verbose = pflag.Bool("verbose", false, "Show logs")
+	cfgfile   = pflag.String("config", "", "Config file to use")
+	verbose   = pflag.Bool("verbose", false, "Show logs")
 )
 
 func main() {
@@ -35,7 +38,6 @@ func main() {
 		fmt.Println("Error reading config:", err)
 		os.Exit(1)
 	}
-	viper.Set("client.ServiceName", "netauth")
 
 	// Shut off all the logging
 	if !*verbose {
@@ -43,22 +45,22 @@ func main() {
 		log.SetOutput(ioutil.Discard)
 	}
 
-	// Grab a client
-	c, err := client.New()
+	viper.Set("token.cache", "memory")
+	c, err := netauth.New()
 	if err != nil {
-		fmt.Println("Client initialization error:", err)
+		log.Println("Error during client initialization:", err)
 		os.Exit(1)
 	}
 
 	// Set the service ID
-	viper.Set("client.ServiceName", *serviceID)
+	c.SetServiceName(*serviceID)
 
-	e, err := c.EntityInfo(*entityID)
+	e, err := c.EntityInfo(context.Background(), *entityID)
 	if err != nil {
 		log.Println("Error loading entity:", err)
 		os.Exit(1)
 	}
-	if e.Meta != nil && e.GetMeta().GetLocked() {
+	if e.GetMeta().GetLocked() {
 		// If locked metadata is present, then don't return
 		// anything.
 		os.Exit(0)
@@ -66,14 +68,14 @@ func main() {
 
 	// This is only ever done for read, never write, so we feed a
 	// null token
-	keys, err := c.ModifyEntityKeys("", *entityID, "LIST", *keyType, "")
+	keys, err := c.EntityKeys(context.Background(), *entityID, "READ", *keyType, "")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
 	}
 
 	// Print out the keys, no formatting, just the key data
-	for _, k := range keys {
+	for _, k := range keys[strings.ToUpper(*keyType)] {
 		fmt.Println(k)
 	}
 	os.Exit(0)
